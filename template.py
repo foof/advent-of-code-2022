086377faee18b35484bf441847f4d5013948b4e1
Jonas Holmström <jonas@holmstrom.me>
2022-12-01 20:18:36 +0100

    Added template file

---
+++ template.py
@@ -0,0 +1,14 @@
+
+def part1(lines):
+    pass
+
+
+def part2(lines):
+    pass
+
+
+if __name__ == "__main__":
+    with open('./input', 'r') as f:
+        lines = f.read().splitlines()
+        print(part1(lines))
+        print(part2(lines))
