59143654b5703010b801acb454aca42023d726b9
Jonas Holmström <jonas@holmstrom.me>
2022-12-06 11:14:38 +0100

    remove comments

--- 06/main.py
+++ 06/main.py
@@ -1,12 +1,9 @@
 
 def find_position(line, num_unique):
     for i in range(num_unique, len(line)):
-        # print("pos", i)
         S = set()
         for j in range(1, num_unique+1):
-            # print(i-j)
             S.add(line[i-j])
-        # print('---------')
         if len(S) == num_unique:
             return i
 
